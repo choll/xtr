0d98f92dd205eb0b9d5bc5ea2c7f7cc6df3aa521
choll <c.holloway@gmail.com>
2021-05-30 21:49:24 +0100

    detect thread sanitizer under gcc correctly

--- include/xtr/detail/config.hpp
+++ include/xtr/detail/config.hpp
@@ -1,7 +1,9 @@
 #ifndef XTR_DETAIL_CONFIG_HPP
 #define XTR_DETAIL_CONFIG_HPP
 
-#if defined(__has_feature)
+#if defined(__SANITIZE_THREAD__)
+#define XTR_THREAD_SANITIZER_ENABLED
+#elif defined(__has_feature)
 #if __has_feature(thread_sanitizer)
 #define XTR_THREAD_SANITIZER_ENABLED
 #endif
