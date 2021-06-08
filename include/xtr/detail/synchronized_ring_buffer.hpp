choll/xtr

commit 884d0c5684a7a199a2939f3cf0f4144eea6079c3

Add log levels plus command tool to examine/modify levels, reopen the log file etc

include/xtr/detail/synchronized_ring_buffer.hpp
@@ -21,6 +21,7 @@
 #ifndef XTR_DETAIL_SYNCHRONIZED_RING_BUFFER_HPP
 #define XTR_DETAIL_SYNCHRONIZED_RING_BUFFER_HPP
 
+#include "config.hpp"
 #include "tags.hpp"
 #include "mirrored_memory_mapping.hpp"
 #include "pagesize.hpp"
@@ -89,7 +90,7 @@ namespace xtr::detail
     static_assert(std::is_same<std::uint64_t, least_uint_t<18446744073709551615UL>>::value);
 #endif
 
-    // XXX Put in detail/ directory
+    // XXX Put in detail/ directory? explain non-use of std::span?
     // Probably ring buffer should be in detail too,
     // everything except the logger really
     template<typename T, typename SizeType>
@@ -160,27 +161,29 @@ public:
     using iterator = std::byte*;
     using const_iterator = const std::byte*;
     using size_type = std::size_t;
-
-    //using span = yeti::span<std::byte>;
-    //using const_span = yeti::span<const std::byte>;
-
     using span = detail::span<std::byte, size_type>;
     using const_span = detail::span<const std::byte, size_type>;
 
     static constexpr bool is_dynamic = Capacity == dynamic_capacity;
 
 public:
-    synchronized_ring_buffer()
+    synchronized_ring_buffer(
+        int fd = -1,
+        std::size_t offset = 0,
+        int flags = srb_flags)
+    requires (!is_dynamic)
     {
-        if (!is_dynamic)
-        {
-            m_ = mirrored_memory_mapping{capacity(), -1, 0, srb_flags};
-            nread_plus_capacity_ = wrnread_plus_capacity_ = capacity();
-            wrbase_ = m_.get();
-        }
+        m_ = mirrored_memory_mapping{capacity(), fd, offset, flags};
+        nread_plus_capacity_ = wrnread_plus_capacity_ = capacity();
+        wrbase_ = begin();
     }
 
-    explicit synchronized_ring_buffer(size_type min_capacity)
+    explicit synchronized_ring_buffer(
+        size_type min_capacity,
+        int fd = -1,
+        std::size_t offset = 0,
+        int flags = srb_flags)
+    requires is_dynamic
     :
         m_(
             align_to_page_size(
@@ -189,11 +192,12 @@ public:
 #else
                 std::ceil2(min_capacity)),
 #endif
-            -1, 0, srb_flags)
+            fd,
+            offset,
+            flags)
     {
-        static_assert(is_dynamic);
         assert(capacity() <= std::numeric_limits<size_type>::max());
-        wrbase_ = m_.get();
+        wrbase_ = begin();
         wrcapacity_ = capacity();
         nread_plus_capacity_ = wrnread_plus_capacity_ = capacity();
     }
@@ -273,7 +277,7 @@ public:
         // be read from the buffer.
         const size_type nr =
             nread_plus_capacity_.load(std::memory_order_relaxed) - capacity();
-        const auto b = m_.get() + clamp(nr, capacity());
+        const auto b = begin() + clamp(nr, capacity());
         // This acquire pairs with the release in reduce_writable(). No reads or
         // writes in the current thread can be reordered before this load.
         const size_type sz = nwritten_.load(std::memory_order_acquire) - nr;
@@ -288,12 +292,14 @@ public:
         // This release pairs with the acquire in write_span(). No reads or writes
         // in the current thread can be reordered after this store.
         nread_plus_capacity_.fetch_add(nbytes, std::memory_order_release);
+#if !defined(XTR_THREAD_SANITIZER_ENABLED)
         assert(nread_plus_capacity_.load() - nwritten_.load() <= capacity());
+#endif
     }
 
     iterator begin() noexcept
     {
-        return m_.get();
+        return static_cast<iterator>(m_.get());
     }
 
     iterator end() noexcept
@@ -303,7 +309,7 @@ public:
 
     const_iterator begin() const noexcept
     {
-        return m_.get();
+        return static_cast<const_iterator>(m_.get());
     }
 
     const_iterator end() const noexcept
@@ -375,4 +381,3 @@ private:
 };
 
 #endif
-

src/logger.cpp
@@ -19,26 +19,54 @@
 // SOFTWARE.
 
 #include "xtr/logger.hpp"
+#include "xtr/detail/commands/command_dispatcher.hpp"
+#include "xtr/detail/commands/matcher.hpp"
+#include "xtr/detail/commands/requests.hpp"
+#include "xtr/detail/commands/responses.hpp"
+#include "xtr/detail/config.hpp"
+#include "xtr/detail/strzcpy.hpp"
+#include "xtr/detail/tsc.hpp"
 
 #include <fmt/chrono.h>
 
 #include <algorithm>
-#include <condition_variable>
+#include <climits>
 #include <cstring>
+#include <condition_variable>
+
+XTR_FUNC
+xtr::logger::~logger()
+{
+    control_.close();
+}
+
+XTR_FUNC
+std::string xtr::logger::default_command_path()
+{
+    static std::atomic<unsigned> ctl_count{0};
+    const long pid = ::getpid();
+    const unsigned long uid = ::geteuid();
+    const unsigned n = ctl_count++;
+    char path[PATH_MAX];
+    std::snprintf(path, sizeof(path), "/run/user/%lu/xtrctl.%ld.%u", uid, pid, n);
+    return path;
+}
 
 XTR_FUNC
 xtr::logger::producer xtr::logger::get_producer(std::string name)
 {
-    return producer{*this, std::move(name)};
+    return producer(*this, std::move(name));
 }
 
 XTR_FUNC
-void xtr::logger::register_producer(producer& p) noexcept
+void xtr::logger::register_producer(
+    producer& p,
+    const std::string& name) noexcept
 {
     post(
-        [&p](state& st)
+        [&p, name](consumer& c, auto&)
         {
-            st.producers.push_back(&p);
+            c.add_producer(p, name);
         });
 }
 
@@ -55,30 +83,43 @@ void xtr::logger::set_error_stream(FILE* stream) noexcept
 }
 
 XTR_FUNC
-void xtr::logger::consumer(state st, std::function<::timespec()> clock) noexcept
+void xtr::logger::set_command_path(std::string path) noexcept
+{
+    post([p = std::move(path)](consumer& c, auto&) { c.set_command_path(std::move(p)); });
+    control_.sync();
+}
+
+XTR_FUNC
+void xtr::logger::consumer::run(std::function<::timespec()> clock) noexcept
 {
     char ts[32] = {};
     bool ts_stale = true;
     std::size_t flush_count = 0;
     fmt::memory_buffer mbuf;
 
-    for (std::size_t i = 0; !st.producers.empty(); ++i)
+    for (std::size_t i = 0; !producers_.empty(); ++i)
     {
         ring_buffer::span span;
         // The inner loop below can modify producers so a reference cannot be taken
-        const std::size_t n = i % st.producers.size();
-        // Read the clock once per loop over producers
-        ts_stale |= n == 0;
+        const std::size_t n = i % producers_.size();
 
-        if ((span = st.producers[n]->buf_.read_span()).empty())
+        if (n == 0)
+        {
+            // Read the clock and commands once per loop over producers
+            ts_stale |= true;
+            if (cmds_)
+                cmds_->process_commands(/* timeout= */0);
+        }
+
+        if ((span = producers_[n]->buf_.read_span()).empty())
         {
             // flush if no further data available (all producers empty)
             if (flush_count != 0 && flush_count-- == 1)
-                st.flush();
+                flush();
             continue;
         }
 
-        st.destroy = false;
+        destroy = false;
 
         if (ts_stale)
         {
@@ -88,74 +129,229 @@ void xtr::logger::consumer(state st, std::function<::timespec()> clock) noexcept
 
         // span.end is capped to the end of the first mapping to guarantee that
         // data is only read from the same address that it was written to (the
-        // producer always begins log records in the first mapping, so do not
+        // producer always begins log records in the first mapping, so we do not
         // read a record beginning in the second mapping). This is done to
         // avoid undefined behaviour---reading an object from a different
         // address than it was written to will work on Intel and probably many
         // other CPUs but is outside of what is permitted by the C++ memory
         // model.
         std::byte* pos = span.begin();
-        std::byte* end = std::min(span.end(), st.producers[n]->buf_.end());
+        std::byte* end = std::min(span.end(), producers_[n]->buf_.end());
         do
         {
             assert(std::uintptr_t(pos) % alignof(fptr_t) == 0);
             fptr_t fptr = *reinterpret_cast<const fptr_t*>(pos);
-            pos = fptr(mbuf, pos, st, ts, st.producers[n]->name_);
-            if (st.destroy)
-            {
-                using std::swap;
-                swap(st.producers[n], st.producers.back()); // possible self-swap, ok
-                st.producers.pop_back();
-                goto next;
-            }
-        } while (pos < end);
+            pos = fptr(mbuf, pos, *this, ts, producers_[n].name);
+        } while (pos < end && !destroy);
 
-        st.producers[n]->buf_.reduce_readable(
+        producers_[n]->buf_.reduce_readable(
             ring_buffer::size_type(pos - span.begin()));
 
+        if (destroy)
+        {
+            using std::swap;
+            swap(producers_[n], producers_.back()); // possible self-swap, ok
+            producers_.pop_back();
+            continue;
+        }
+
         std::size_t n_dropped;
-        if (st.producers[n]->buf_.read_span().empty() &&
-            (n_dropped = st.producers[n]->buf_.dropped_count()) > 0)
+        if (producers_[n]->buf_.read_span().empty() &&
+            (n_dropped = producers_[n]->buf_.dropped_count()) > 0)
         {
             detail::print(
                 mbuf,
-                st.out,
-                st.err,
-                "{}: {}: {} messages dropped\n",
+                out,
+                err,
+                "W {} {}: {} messages dropped\n",
                 ts,
-                st.producers[n]->name_,
+                producers_[n].name,
                 n_dropped);
+            producers_[n].dropped_count += n_dropped;
         }
 
         // flushing may be late/early if producers is modified, doesn't matter
-        flush_count = st.producers.size();
-
-        next:;
+        flush_count = producers_.size();
     }
+
+    close();
+}
+
+XTR_FUNC
+void xtr::logger::consumer::add_producer(producer& p, const std::string& name)
+{
+    producers_.push_back(producer_handle{&p, name});
+}
+
+XTR_FUNC
+void xtr::logger::consumer::set_command_path(std::string path) noexcept
+{
+    cmds_.reset(new detail::command_dispatcher(std::move(path)));
+
+    // Reset if failed to open
+    if (!cmds_->is_open())
+    {
+        cmds_.reset();
+        return;
+    }
+
+    // Set commands
+    cmds_->register_callback<detail::status>(
+        std::bind_front(&consumer::status_handler, this));
+
+    cmds_->register_callback<detail::set_level>(
+        std::bind_front(&consumer::set_level_handler, this));
+
+    cmds_->register_callback<detail::reopen>(
+        std::bind_front(&consumer::reopen_handler, this));
+}
+
+XTR_FUNC
+void xtr::logger::consumer::status_handler(int fd, detail::status& st)
+{
+    st.pattern.text[sizeof(st.pattern.text) - 1] = '\0';
+
+    const auto matcher =
+        detail::make_matcher(
+            st.pattern.type, st.pattern.text, st.pattern.ignore_case);
+
+    if (!matcher->valid())
+    {
+        detail::frame<detail::error> ef;
+        matcher->error_reason(ef->reason, sizeof(ef->reason));
+        cmds_->send(fd, ef);
+        return;
+    }
+
+    for (std::size_t i = 1; i < producers_.size(); ++i)
+    {
+        auto& p = producers_[i];
+
+        if (!(*matcher)(p.name.c_str()))
+            continue;
+
+        detail::frame<detail::sink_info> sif;
+
+        sif->level = p->level();
+        sif->buf_capacity = p->buf_.capacity();
+        sif->buf_nbytes = p->buf_.read_span().size();
+        sif->dropped_count = p.dropped_count;
+        detail::strzcpy(sif->name, p.name);
+
+        cmds_->send(fd, sif);
+    }
+}
+
+XTR_FUNC
+void xtr::logger::consumer::set_level_handler(int fd, detail::set_level& sl)
+{
+    sl.pattern.text[sizeof(sl.pattern.text) - 1] = '\0';
+
+    if (sl.level > xtr::log_level_t::debug)
+    {
+        cmds_->send_error(fd, "Invalid level");
+        return;
+    }
+
+    const auto matcher =
+        detail::make_matcher(
+            sl.pattern.type, sl.pattern.text, sl.pattern.ignore_case);
+
+    if (!matcher->valid())
+    {
+        detail::frame<detail::error> ef;
+        matcher->error_reason(ef->reason, sizeof(ef->reason));
+        cmds_->send(fd, ef);
+        return;
+    }
+
+    for (std::size_t i = 1; i < producers_.size(); ++i)
+    {
+        auto& p = producers_[i];
+
+        if (!(*matcher)(p.name.c_str()))
+            continue;
+
+        p->set_level(sl.level);
+    }
+
+    cmds_->send(fd, detail::frame<detail::success>());
+}
+
+XTR_FUNC
+void xtr::logger::consumer::reopen_handler(int fd, detail::reopen&)
+{
+    if (!reopen())
+        cmds_->send_error(fd, std::strerror(errno));
+    else
+        cmds_->send(fd, detail::frame<detail::success>());
+}
+
+XTR_FUNC
+xtr::logger::producer::producer(const producer& other)
+{
+    *this = other;
+}
+
+XTR_FUNC
+xtr::logger::producer& xtr::logger::producer::operator=(const producer& other)
+{
+    level_ = other.level_.load(std::memory_order_relaxed);
+    if (!std::exchange(open_, other.open_)) // if previously closed, register
+    {
+        const_cast<producer&>(other).post(
+            [this](consumer& c, const auto& name) { c.add_producer(*this, name); });
+    }
+    return *this;
 }
 
 XTR_FUNC
 xtr::logger::producer::producer(logger& owner, std::string name)
 :
-    name_(std::move(name))
+    open_(true)
 {
-    owner.register_producer(*this);
+    owner.register_producer(*this, name);
 }
 
 XTR_FUNC
-void xtr::logger::producer::sync(bool destructing)
+void xtr::logger::producer::close()
+{
+    if (open_)
+    {
+        sync(/*destruct=*/true);
+        open_ = false;
+#if defined(XTR_THREAD_SANITIZER_ENABLED)
+    // This is done to prevent thread sanitizer complaining that the last
+    // release operation performed by the consumer was not synchronized with a
+    // corresponding acquire operation. This will happen if the memory used
+    // for synchronized_ring_buffer's atomic variables are accessed
+    // non-atomically, e.g. by free(3) or when stack addresses are reused.
+    // As the last operation the consumer calls is reduce_readable, we must
+    // call write_span to 'balance' the atomic operations.
+
+    // Wait for consumer to perform the offending release
+    while (buf_.read_span().size() > 0)
+        ;
+    // Perform matching acquire operation
+    buf_.write_span();
+#endif
+    }
+}
+
+XTR_FUNC
+void xtr::logger::producer::sync(bool destroy)
 {
     std::condition_variable cv;
     std::mutex m;
-    bool notified = false;
+    bool notified = false; // protected by m
 
     post(
-        [&cv, &m, &notified, destructing](state& st)
+        [&cv, &m, &notified, destroy](consumer& c, auto&)
         {
-            st.destroy = destructing;
+            c.destroy = destroy;
 
-            st.flush();
-            st.sync();
+            c.flush();
+            c.sync();
 
             std::scoped_lock lock{m};
             notified = true;
@@ -193,9 +389,9 @@ XTR_FUNC
 void xtr::logger::producer::set_name(std::string name)
 {
     post(
-        [this, name{std::move(name)}](state&)
+        [name = std::move(name)](auto&, auto& oldname)
         {
-            this->name_ = std::move(name);
+            oldname = std::move(name);
         });
     sync();
 }
@@ -203,6 +399,5 @@ void xtr::logger::producer::set_name(std::string name)
 XTR_FUNC
 xtr::logger::producer::~producer()
 {
-    sync(true);
+    close();
 }
-

include/xtr/detail/clock_ids.hpp
@@ -22,5 +22,13 @@
 // Clock source used to translate TSC to wall time
 #define XTR_CLOCK_WALL CLOCK_REALTIME
 
+// Clock source used internally
+#if defined(CLOCK_MONOTONIC_COARSE)
+#define XTR_CLOCK_MONOTONIC_FAST CLOCK_MONOTONIC_COARSE
+#elif defined (CLOCK_MONOTONIC_FAST)
+#define XTR_CLOCK_MONOTONIC_FAST CLOCK_MONOTONIC_FAST
+#else
+#define XTR_CLOCK_MONOTONIC_FAST CLOCK_MONOTONIC
 #endif
 
+#endif

include/xtr/detail/commands/command_dispatcher.hpp
@@ -1,0 +1,125 @@
+// Copyright 2020 Chris E. Holloway
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in all
+// copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+// SOFTWARE.
+
+#ifndef XTR_DETAIL_COMMANDS_COMMAND_DISPATCHER_HPP
+#define XTR_DETAIL_COMMANDS_COMMAND_DISPATCHER_HPP
+
+#include "command_dispatcher_fwd.hpp"
+#include "frame.hpp"
+#include "xtr/detail/file_descriptor.hpp"
+
+#include <cstddef>
+#include <functional>
+#include <memory>
+#include <string>
+#include <unordered_map>
+#include <vector>
+
+#include <poll.h>
+
+namespace xtr::detail
+{
+    class command_dispatcher;
+}
+
+class xtr::detail::command_dispatcher
+{
+private:
+    struct pollfd
+    {
+        detail::file_descriptor fd;
+        short events;
+        short revents;
+    };
+
+    static_assert(sizeof(pollfd) == sizeof(::pollfd));
+    static_assert(alignof(pollfd) == alignof(::pollfd));
+
+    struct buffer
+    {
+        buffer(const void* srcbuf, std::size_t srcsize);
+
+        std::unique_ptr<char[]> buf;
+        std::size_t size;
+    };
+
+    struct callback_result
+    {
+        std::vector<buffer> bufs;
+        std::size_t pos = 0;
+    };
+
+    struct callback
+    {
+        std::function<void(int, void*)> func;
+        std::size_t size;
+    };
+
+public:
+    explicit command_dispatcher(std::string path);
+
+    ~command_dispatcher();
+
+    template<typename Payload, typename Callback>
+    void register_callback(Callback&& c)
+    {
+        using frame_type = detail::frame<Payload>;
+
+        static_assert(sizeof(frame_type) <= max_frame_size);
+        static_assert(alignof(frame_type) <= max_frame_alignment);
+
+        callbacks_[Payload::frame_id] =
+            callback{
+                [c = std::forward<Callback>(c)](int fd, void* buf)
+                {
+                    c(fd, static_cast<frame_type*>(buf)->payload);
+                },
+                sizeof(frame_type)};
+    }
+
+    void send(int fd, const void* buf, std::size_t nbytes);
+
+    template<typename FrameType>
+    void send(int fd, const FrameType& frame)
+    {
+        send(fd, &frame, sizeof(frame));
+    }
+
+    void send_error(int fd, std::string_view reason);
+
+    void process_commands(int timeout) noexcept;
+
+    bool is_open() const noexcept
+    {
+        return !pollfds_.empty();
+    }
+
+private:
+    void process_socket_read(pollfd& pfd) noexcept;
+    void process_socket_write(pollfd& pfd) noexcept;
+    void disconnect(pollfd& pfd) noexcept;
+
+    std::unordered_map<frame_id_t, callback> callbacks_;
+    std::vector<pollfd> pollfds_;
+    std::unordered_map<int, callback_result> results_;
+    std::string path_;
+};
+
+#endif

include/xtr/detail/commands/command_dispatcher_fwd.hpp
@@ -1,4 +1,4 @@
-// Copyright 2014, 2015, 2019 Chris E. Holloway
+// Copyright 2020 Chris E. Holloway
 //
 // Permission is hereby granted, free of charge, to any person obtaining a copy
 // of this software and associated documentation files (the "Software"), to deal
@@ -18,14 +18,17 @@
 // OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 // SOFTWARE.
 
-#ifndef XTR_DETAIL_PAGESIZE_HPP
-#define XTR_DETAIL_PAGESIZE_HPP
-
-#include <cstddef>
+#ifndef XTR_DETAIL_COMMAND_DISPATCHER_FWD_HPP
+#define XTR_DETAIL_COMMAND_DISPATCHER_FWD_HPP
 
 namespace xtr::detail
 {
-    std::size_t align_to_page_size(std::size_t length);
+    class command_dispatcher;
+
+    struct command_dispatcher_deleter
+    {
+        void operator()(command_dispatcher*);
+    };
 }
 
 #endif

include/xtr/detail/commands/connect.hpp
@@ -1,4 +1,4 @@
-// Copyright 2014, 2015, 2019 Chris E. Holloway
+// Copyright 2021 Chris E. Holloway
 //
 // Permission is hereby granted, free of charge, to any person obtaining a copy
 // of this software and associated documentation files (the "Software"), to deal
@@ -18,14 +18,54 @@
 // OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 // SOFTWARE.
 
-#ifndef XTR_DETAIL_PAGESIZE_HPP
-#define XTR_DETAIL_PAGESIZE_HPP
+#ifndef XTR_DETAIL_COMMANDS_CONNECT_HPP
+#define XTR_DETAIL_COMMANDS_CONNECT_HPP
 
-#include <cstddef>
+#include "xtr/detail/file_descriptor.hpp"
+#include "xtr/detail/strzcpy.hpp"
+
+#include <string_view>
+
+#include <sys/socket.h>
+#include <sys/un.h>
 
 namespace xtr::detail
 {
-    std::size_t align_to_page_size(std::size_t length);
+    [[nodiscard]] file_descriptor command_connect(std::string_view path);
+}
+
+inline xtr::detail::file_descriptor xtr::detail::command_connect(std::string_view path)
+{
+    file_descriptor fd(::socket(AF_LOCAL, SOCK_SEQPACKET, 0));
+
+    if (!fd)
+        return {};
+
+    sockaddr_un addr;
+    addr.sun_family = AF_LOCAL;
+
+    if (path.size() >= sizeof(addr.sun_path))
+    {
+        errno = ENAMETOOLONG;
+        return {};
+    }
+
+    strzcpy(addr.sun_path, path);
+
+#if defined(__linux__)
+    if (addr.sun_path[0] == '\0') // abstract socket
+    {
+        std::memset(
+            addr.sun_path + path.size(),
+            '\0',
+            sizeof(addr.sun_path) - path.size());
+    }
+#endif
+
+    if (::connect(fd.get(), reinterpret_cast<sockaddr*>(&addr), sizeof(addr)) != 0)
+        return {};
+
+    return fd;
 }
 
 #endif

include/xtr/detail/commands/frame.hpp
@@ -1,4 +1,4 @@
-// Copyright 2014, 2019 Chris E. Holloway
+// Copyright 2021 Chris E. Holloway
 //
 // Permission is hereby granted, free of charge, to any person obtaining a copy
 // of this software and associated documentation files (the "Software"), to deal
@@ -18,40 +18,55 @@
 // OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 // SOFTWARE.
 
-#ifndef XTR_DETAIL_ALIGN_HPP
-#define XTR_DETAIL_ALIGN_HPP
+#ifndef XTR_DETAIL_COMMANDS_FRAME_HPP
+#define XTR_DETAIL_COMMANDS_FRAME_HPP
 
-#include <bit>
-#include <cassert>
 #include <cstddef>
 #include <cstdint>
 #include <type_traits>
 
 namespace xtr::detail
 {
-    // value is unchanged if it is already aligned
-    template<typename T>
-    constexpr T align(T value, T alignment) noexcept
-    {
-        static_assert(std::is_unsigned_v<T>, "value must be unsigned");
-        assert(std::has_single_bit(alignment));
-        return (value + (alignment - 1)) & ~(alignment - 1);
-    }
+    using frame_id_t = std::uint32_t;
 
-    // Align is only a template argument to allow it to be used with
-    // assume_aligned. Improvement: Make Align a plain argument if it is
-    // possible to do so while still marking the return value as aligned.
-    template<std::size_t Align, typename T>
-    __attribute__((assume_aligned(Align))) T* align(T* ptr) noexcept
+    struct frame_header
     {
-        static_assert(
-            std::is_same_v<std::remove_cv_t<T>, std::byte> ||
-            std::is_same_v<std::remove_cv_t<T>, char> ||
-            std::is_same_v<std::remove_cv_t<T>, unsigned char> ||
-            std::is_same_v<std::remove_cv_t<T>, signed char>,
-            "value must be a char or byte pointer");
-        return reinterpret_cast<T*>(align(std::uintptr_t(ptr), Align));
-    }
+        frame_id_t frame_id;
+    };
+
+    inline constexpr std::size_t max_frame_alignment = alignof(std::max_align_t);
+    inline constexpr std::size_t max_frame_size = 512;
+
+    union alignas(max_frame_alignment) frame_buf
+    {
+        frame_header hdr;
+        char buf[max_frame_size];
+    };
+
+    template<typename Payload>
+    struct frame : frame_header
+    {
+        static_assert(std::is_standard_layout_v<Payload>);
+        static_assert(std::is_trivially_destructible_v<Payload>);
+        static_assert(std::is_trivially_copyable_v<Payload>);
+
+        using payload_type = Payload;
+
+        frame()
+        {
+            static_assert(alignof(frame<Payload>) <= max_frame_alignment);
+            static_assert(sizeof(frame<Payload>) <= max_frame_size);
+
+            frame_id = Payload::frame_id;
+        }
+
+        Payload* operator->()
+        {
+            return &payload;
+        }
+
+        [[no_unique_address]] Payload payload{};
+    };
 }
 
 #endif

commit de6fe9040073d59611f134da700966cdc210354e

fix bug introduced in 884d0c5; reduce_readable was being called on producers after the destroy flag is set

include/xtr/detail/synchronized_ring_buffer.hpp
@@ -202,6 +202,15 @@ public:
         nread_plus_capacity_ = wrnread_plus_capacity_ = capacity();
     }
 
+    void clear() noexcept
+    {
+        nwritten_ = 0;
+        wrnread_plus_capacity_ = capacity();
+        wrnwritten_ = 0;
+        nread_plus_capacity_ = capacity();
+        dropped_count_ = 0;
+    }
+
     constexpr size_type capacity() const noexcept
     {
         if constexpr (is_dynamic)

src/logger.cpp
@@ -23,7 +23,6 @@
 #include "xtr/detail/commands/matcher.hpp"
 #include "xtr/detail/commands/requests.hpp"
 #include "xtr/detail/commands/responses.hpp"
-#include "xtr/detail/config.hpp"
 #include "xtr/detail/strzcpy.hpp"
 #include "xtr/detail/tsc.hpp"
 
@@ -128,12 +127,10 @@ void xtr::logger::consumer::run(std::function<::timespec()> clock) noexcept
         do
         {
             assert(std::uintptr_t(pos) % alignof(fptr_t) == 0);
+            assert(!destroy);
             fptr_t fptr = *reinterpret_cast<const fptr_t*>(pos);
             pos = fptr(mbuf, pos, *this, ts, producers_[n].name);
-        } while (pos < end && !destroy);
-
-        producers_[n]->buf_.reduce_readable(
-            ring_buffer::size_type(pos - span.begin()));
+        } while (pos < end);
 
         if (destroy)
         {
@@ -142,6 +139,9 @@ void xtr::logger::consumer::run(std::function<::timespec()> clock) noexcept
             producers_.pop_back();
             continue;
         }
+
+        producers_[n]->buf_.reduce_readable(
+            ring_buffer::size_type(pos - span.begin()));
 
         std::size_t n_dropped;
         if (producers_[n]->buf_.read_span().empty() &&
@@ -311,21 +311,14 @@ void xtr::logger::producer::close()
     {
         sync(/*destruct=*/true);
         open_ = false;
-#if defined(XTR_THREAD_SANITIZER_ENABLED)
-    // This is done to prevent thread sanitizer complaining that the last
-    // release operation performed by the consumer was not synchronized with a
-    // corresponding acquire operation. This will happen if the memory used
-    // for synchronized_ring_buffer's atomic variables are accessed
-    // non-atomically, e.g. by free(3) or when stack addresses are reused.
-    // As the last operation the consumer calls is reduce_readable, we must
-    // call write_span to 'balance' the atomic operations.
-
-    // Wait for consumer to perform the offending release
-    while (buf_.read_span().size() > 0)
-        ;
-    // Perform matching acquire operation
-    buf_.write_span();
-#endif
+        // clear() is called here in case the producer is registered with the
+        // logger again, e.g. via assignment. This is because when the
+        // 'destruct' flag is received by the consumer it cannot perform any
+        // further operations on the producer (as the producer may no longer
+        // exist), including updating the read offset of the ring buffer, which
+        // means that some residual data will be left in the buffer that needs
+        // to be cleared.
+        buf_.clear();
     }
 }
 