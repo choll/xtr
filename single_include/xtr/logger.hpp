a0f3c1fe27f93ecee9593647346562b53244d02d
choll <c.holloway@gmail.com>
2022-03-26 20:49:38 +0000

    fix single include header

--- single_include/xtr/logger.hpp
+++ single_include/xtr/logger.hpp
@@ -25,6 +25,37 @@ SOFTWARE.
 #ifndef XTR_LOGGER_HPP
 #define XTR_LOGGER_HPP
 
+/**
+ * Sets the capacity (in bytes) of the queue that sinks use to send log data
+ * to the background thread. Each sink will have an individual queue of this
+ * size. Users are permitted to define this variable in order to set a custom
+ * capacity. User provided capacities may be rounded up\---to obtain the
+ * actual capacity invoke @ref xtr::sink::capacity.
+ */
+#if !defined(XTR_SINK_CAPACITY)
+#define XTR_SINK_CAPACITY (256 * 1024)
+#endif
+
+/**
+ */
+#if !defined(XTR_FORCE_TSC_CALIBRATION)
+#define XTR_FORCE_TSC_CALIBRATION 0
+#endif
+
+/**
+ */
+#if !defined(XTR_USE_IO_URING)
+#define XTR_USE_IO_URING __has_include(<liburing.h>)
+#endif
+
+#define XTR_IO_URING_POLL 0
+
+/**
+ */
+#if !defined(XTR_IO_URING_POLL)
+#define XTR_IO_URING_POLL 1
+#endif
+
 #include <ctime>
 
 #include <fmt/chrono.h>
@@ -1427,37 +1458,6 @@ namespace xtr::detail
         dst[n] = '\0';
     }
 }
-
-/**
- * Sets the capacity (in bytes) of the queue that sinks use to send log data
- * to the background thread. Each sink will have an individual queue of this
- * size. Users are permitted to define this variable in order to set a custom
- * capacity. User provided capacities may be rounded up\---to obtain the
- * actual capacity invoke @ref xtr::sink::capacity.
- */
-#if !defined(XTR_SINK_CAPACITY)
-#define XTR_SINK_CAPACITY (256 * 1024)
-#endif
-
-/**
- */
-#if !defined(XTR_FORCE_TSC_CALIBRATION)
-#define XTR_FORCE_TSC_CALIBRATION 0
-#endif
-
-/**
- */
-#if !defined(XTR_USE_IO_URING)
-#define XTR_USE_IO_URING __has_include(<liburing.h>)
-#endif
-
-#define XTR_IO_URING_POLL 0
-
-/**
- */
-#if !defined(XTR_IO_URING_POLL)
-#define XTR_IO_URING_POLL 1
-#endif
 
 #include <atomic>
 #include <cassert>
